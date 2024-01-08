709acb0ecc24c7e5f5b1d14aab4cbfb855a02bd0
aminyous <amyn.hichri@gmail.com>
2024-01-08 16:11:21 +0100

    Read and write to/from a file exercice

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-my_file = open("data.txt", "r")
-file_content = my_file.read()
+friends_list = []
+
+for i in range(3):
+    name = input("Enter your friend's name: ")
+    friends_list.append(name)
+
+my_file = open("people.txt", "r")
+file_content = [item.strip() for item in my_file.readlines()]
 my_file.close()
 
-print(file_content)
+friends_list_set = set(friends_list)
+file_content_set = set(file_content)
 
-user_name = input("Enter your name: ")
-my_file_writing = open("data.txt", "w")
-my_file_writing.write(user_name)
+nearby_friends = friends_list_set.intersection(file_content_set)
+nearby_friends_list = list(nearby_friends)
+
+nearby_friends_list = [item + "\n" for item in nearby_friends_list]
+
+my_file_writing = open("nearby_friends.txt", "w")
+my_file_writing.writelines(nearby_friends_list)
 my_file_writing.close()
+
+
+
+
+
+
+
+
