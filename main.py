410d6482f8c265f0f2096d4545f23bea67290c86
aminyous <amyn.hichri@gmail.com>
2024-01-09 16:34:39 +0100

    Load a json file into python code and convert it to a dict

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-friends_list = []
+import json
 
-for i in range(3):
-    name = input("Enter your friend's name: ")
-    friends_list.append(name)
+file = open("friends_json.txt", "r")
+file_content = json.load(file)  # file_content is a dict now
+file.close()
 
-my_file = open("people.txt", "r")
-file_content = [item.strip() for item in my_file.readlines()]
-my_file.close()
-
-friends_list_set = set(friends_list)
-file_content_set = set(file_content)
-
-nearby_friends = friends_list_set.intersection(file_content_set)
-nearby_friends_list = list(nearby_friends)
-
-nearby_friends_list = [item + "\n" for item in nearby_friends_list]
-
-my_file_writing = open("nearby_friends.txt", "w")
-my_file_writing.writelines(nearby_friends_list)
-my_file_writing.close()
-
+print(file_content["friends"])
 
 
 
