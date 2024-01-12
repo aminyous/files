05537c7047df1dd686bc9a5fe90b4ac985488f6e
aminyous <amyn.hichri@gmail.com>
2024-01-12 16:57:55 +0100

    From csv to json file

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import json
 
-file = open("friends_json.txt", "r")
-file_content = json.load(file)  # file_content is a dict now
+file = open("csv_file.txt", "r")
+file_content = [item.strip() for item in file.readlines()]
 file.close()
 
-print(file_content["friends"])
 
-data = {
-    "name": "John",
-    "age": 30,
-    "city": "New York"
-}
+splitted_content = [item.split(",") for item in file_content]
+dict_item = []
+
+for item in splitted_content:
+    dict_item.append({
+        "club": item[0],
+        "country": item[2],
+        "city": item[1]
+    })
 
 file_dump = open("dict_json.txt", "w")
-json.dump(data, file_dump, indent=2)
+json.dump(dict_item, file_dump, indent=2)
 file_dump.close()
-
-
 
 
 
